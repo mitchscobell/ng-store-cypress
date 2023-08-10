eeb8474133a780cf10c1ca408085b6a0a23b4eb2
Mitch Scobell <mitchscobell@gmail.com>
2023-08-10 09:50:10 -0400

    notes and beforeEach

--- cypress/e2e/demos/D2-contains-license-plates.spec.js
+++ cypress/e2e/demos/D2-contains-license-plates.spec.js
@@ -1,7 +1,12 @@
 describe('License plate store home page', () => {
 
-    it('displays the right main title', () => {
+    // beforeEach is for something that needs to be done for all tests.
+    //          This is helpful for logging on
+    beforeEach(() => {
         cy.visit('http://store.angulartraining.com');
+    });
+
+    it('displays the right main title', () => {
         cy.contains('Welcome to our store')
             .should('be.visible')
             .should('have.css', 'font-weight', '300')
@@ -10,7 +15,6 @@ describe('License plate store home page', () => {
     });
 
     it('displays 8 license plates', () => {
-        cy.visit('http://store.angulartraining.com');
         cy.get('app-license-plate').should('have.length', 8);
     });
 });
