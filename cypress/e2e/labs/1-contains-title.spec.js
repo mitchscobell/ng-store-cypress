7a49cb8834cf725bb07796d1be01718b76e488a6
Mitch Scobell <mitchscobell@gmail.com>
2023-08-10 09:31:20 -0400

    notes about what this lab accomplished

--- cypress/e2e/labs/1-contains-title.spec.js
+++ cypress/e2e/labs/1-contains-title.spec.js
@@ -1,13 +1,19 @@
 describe('License plate store home page', () => {
 
+    /*
+    1) The text "Welcome to our store" is visible
+    2) Its font weight is 300 and font size is 72px
+    3) a tough one: Its font family should match Segoe UI
+    */
+
     it('displays the right main title', () => {
         cy.visit('http://store.angulartraining.com');
         cy.contains('Welcome to our store')
             .should('be.visible')
             .should('have.css', 'font-weight', '300')
             .should('have.css', 'font-size', '72px')
-            .should('have.css', 'font-family') // yields 'sans-serif'
-            .and('match', /Segoe UI/) // yields 'sans-serif'
+            .should('have.css', 'font-family')
+            .and('match', /Segoe UI/)
 
     });
 });
