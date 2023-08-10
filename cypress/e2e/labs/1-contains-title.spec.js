f423ac57026a93176e796dbe252d3a939830f706
Mitch Scobell <mitchscobell@gmail.com>
2023-08-10 09:27:45 -0400

    finished lab 1

--- cypress/e2e/labs/1-contains-title.spec.js
+++ cypress/e2e/labs/1-contains-title.spec.js
@@ -4,7 +4,10 @@ describe('License plate store home page', () => {
         cy.visit('http://store.angulartraining.com');
         cy.contains('Welcome to our store')
             .should('be.visible')
-            .should('have.css', 'font-weight', '300');
+            .should('have.css', 'font-weight', '300')
+            .should('have.css', 'font-size', '72px')
+            .should('have.css', 'font-family') // yields 'sans-serif'
+            .and('match', /Segoe UI/) // yields 'sans-serif'
 
     });
 });
